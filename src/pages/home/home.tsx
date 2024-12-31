091b3581d71faaac5c5499985b1628bfdc4c6a12
Chander Valle <chandez@gmail.com>
2024-12-31 16:11:46 -0500

    Small improvements

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,20 +4,22 @@ const Home = () => {
     <article>
       <h1>Welcome</h1>
       <div className="panel-border-left">
-        <p>
-          Hello. My name is Chander, and I am a software engineer passionate
-          about technology and science.
-        </p>
-        <p>
-          In my free time, I enjoy practicing Jiu-Jitsu, which has taught me
-          discipline, focus, and the importance of continual learning and
-          growth.
-        </p>
-        <p>
-          I have always been fascinated by the power of technology to solve
-          complex problems and improve people's lives. I constantly look for new
-          challenges and opportunities to grow as an engineer.
-        </p>
+        <div className="panel-content">
+          <p>
+            Hello. My name is Chander, and I am a software engineer passionate
+            about technology and science.
+          </p>
+          <p>
+            In my free time, I enjoy practicing Jiu-Jitsu, which has taught me
+            discipline, focus, and the importance of continual learning and
+            growth.
+          </p>
+          <p>
+            I have always been fascinated by the power of technology to solve
+            complex problems and improve people's lives. I constantly look for
+            new challenges and opportunities to grow as an engineer.
+          </p>
+        </div>
       </div>
     </article>
   );
--- src/structure/header/header.tsx
+++ src/structure/header/header.tsx
@@ -5,26 +5,26 @@ import Contact from "../../pages/contact/contact";
 const Header = () => {
   return (
     <Fragment>
-      <header className="main-header">
-        <div className="logo">
-          <Link to="/" className="title">
-            <span>Chander Valle</span>
-            <span>Software Engineer & Web Developer</span>
-          </Link>
+      <header>
+        <div className="main-header">
+          <div className="logo">
+            <Link to="/" className="title">
+              <span>Chander Valle</span>
+              <span>Software Engineer & Web Developer</span>
+            </Link>
+          </div>
+          <nav className="nav">
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/resume">Resume</NavLink>
+          </nav>
         </div>
-        <div className="spacer"></div>
-        <nav className="nav">
-          <NavLink to="/home">Home</NavLink>
-          <NavLink to="/resume">Resume</NavLink>
-        </nav>
       </header>
-      <main className="main">
-        <div className="left-content">
+      <main>
+        <div className="main-content">
           <Outlet />
-        </div>
-        <div className="spacer"></div>
-        <div className="right-content">
-          <Contact></Contact>
+          <div className="right-content">
+            <Contact></Contact>
+          </div>
         </div>
       </main>
     </Fragment>
